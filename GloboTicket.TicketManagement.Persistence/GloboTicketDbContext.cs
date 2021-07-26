673d2726e7e71f44dd7ca1bc505524c0eb0d4f57
Samir Osmanov <samiro@emgage.com>
2021-07-26 15:52:59 +0400

    Unit/Integration tests has been added

--- GloboTicket.TicketManagement.Persistence/GloboTicketDbContext.cs
+++ GloboTicket.TicketManagement.Persistence/GloboTicketDbContext.cs
@@ -10,18 +10,18 @@ namespace GloboTicket.TicketManagement.Persistence
 {
     public class GloboTicketDbContext: DbContext
     {
-        //private readonly ILoggedInUserService _loggedInUserService;
+        private readonly ILoggedInUserService _loggedInUserService;
 
         public GloboTicketDbContext(DbContextOptions<GloboTicketDbContext> options)
            : base(options)
         {
         }
 
-        //public GloboTicketDbContext(DbContextOptions<GloboTicketDbContext> options/*, ILoggedInUserService loggedInUserService*/)
-        //    : base(options)
-        //{
-        //    //_loggedInUserService = loggedInUserService;
-        //}
+        public GloboTicketDbContext(DbContextOptions<GloboTicketDbContext> options, ILoggedInUserService loggedInUserService)
+            : base(options)
+        {
+            _loggedInUserService = loggedInUserService;
+        }
 
         public DbSet<Event> Events { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -202,11 +202,11 @@ namespace GloboTicket.TicketManagement.Persistence
                 {
                     case EntityState.Added:
                         entry.Entity.CreatedDate = DateTime.Now;
-                        //entry.Entity.CreatedBy = _loggedInUserService.UserId;
+                        entry.Entity.CreatedBy = _loggedInUserService.UserId;
                         break;
                     case EntityState.Modified:
                         entry.Entity.LastModifiedDate = DateTime.Now;
-                        //entry.Entity.LastModifiedBy = _loggedInUserService.UserId;
+                        entry.Entity.LastModifiedBy = _loggedInUserService.UserId;
                         break;
                 }
             }
